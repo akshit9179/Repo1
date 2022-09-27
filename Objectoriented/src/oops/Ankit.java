61693e7a342fdf47117681112e3ab0cdf0c461b0
akshit9179 <akshit.anand@kpipartners.com>
2022-09-27 13:57:17 +0530

    my

---
+++ Objectoriented/src/oops/Ankit.java
@@ -0,0 +1,23 @@
+package oops;
+
+public class Ankit implements Skeleton {
+	
+
+public  Ankit() {
+	// Auto-generated constructor stub
+  }
+
+@override
+public void brain( ) {
+	//Todo Auto-generated method stub
+	system.out.println("i AM huNGRY");
+}
+
+@override
+public void flesh() {
+	// Todo Auto-generated method stub
+	system.out.println("i AM NOT FOODIE ")
+}
+
+@override
+public void color() {
